bf23cc925bb6bf5de77282f8f85ab22b3d588f99
Anthony Cueva <hi.cuevantngmail.com>
2023-02-07 13:10:04 -0500

    solve target="_blank" rel="noreferrer"

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -3,10 +3,10 @@ import { Icons } from "@/components/icons"
 
 const SocialLinks = () => (
   <div className="flex space-x-1">
-    <a target="_blank" href={siteConfig.links.instagram}>
+    <a target="_blank" rel="noreferrer" href={siteConfig.links.instagram}>
       <Icons.instagram className="w-5 h-5 mr-2" />
     </a>
-    <a target="_blank" href={siteConfig.links.facebook}>
+    <a target="_blank" rel="noreferrer" href={siteConfig.links.facebook}>
       <Icons.facebook className="w-5 h-5 mr-2 fill-current" />
     </a>
   </div>
